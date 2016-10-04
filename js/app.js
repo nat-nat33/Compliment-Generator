66cf658fb3fe1320a34cab791715dc9390d5d61f
Jon Tan <joanthan.tan83@gmail.com>
2016-10-04 09:43:48 -1000

    updates generate function to make more random, adds google fonts, styled fonts

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 var time = ["today","yesterday","last year","tomorrow","the day before","last month", "this month","next month","this year","next year","forever","sometimes"];
 var action = ["eat","sleep","do homework","watch movies","swim","go shopping","talk","high five","make dinner","squat"];
-var compliment = ["awesome","nice","good","excellent","the best","better than most","like a champ","like superman","like a beautiful rat","like a boss"];
+var goodStuff = ["awesome","nice","good","excellent","the best","better than most","like a champ","like superman","like a beautiful rat","like a boss"];
 
 function generate(){
-  var i = Math.floor(Math.random()*action.length);
-  document.getElementById("compliments").innerHTML = "<div>"+"You"+" "+action[i]+" "+" "+compliment[i]+" "+time[i];
+  // var i = Math.floor(Math.random()*action.length);
+  var when = time[Math.floor(Math.random()*time.length)];
+  var did = action[Math.floor(Math.random()*action.length)];
+  var what = goodStuff[Math.floor(Math.random()*goodStuff.length)];
+  document.getElementById("compliments").innerHTML = "<div>"+"You"+" "+did+" "+" "+what+" "+when;
 }
