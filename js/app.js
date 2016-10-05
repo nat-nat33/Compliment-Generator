be75f0dad6563231823cdf3b3a051b7c2e1cecf9
Jon Tan <joanthan.tan83@gmail.com>
2016-10-04 16:19:03 -1000

    updates compliments to make more sense

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
-var time = ["today","yesterday","last year","tomorrow","the day before","last month", "this month","next month","this year","next year","forever","sometimes"];
+var description = ["magnificently","poetically","beautifully","artistically","lovingly","tenderly","lovingly","positively","excellently","gracefully","delightfully"];
 var action = ["eat","sleep","do homework","watch movies","swim","go shopping","talk","high five","make dinner","squat"];
-var goodStuff = ["awesome","nice","good","excellent","the best","better than most","like a champ","like superman","like a beautiful rat","like a boss"];
+var goodStuff = ["so awesome","very nicely","super good","excellent","the best","better than most","like a champ","like superman","like a beautiful rat","like a boss"];
 
 function generate(){
   // var i = Math.floor(Math.random()*action.length);
-  var when = time[Math.floor(Math.random()*time.length)];
+  var how = description[Math.floor(Math.random()*description.length)];
   var did = action[Math.floor(Math.random()*action.length)];
   var what = goodStuff[Math.floor(Math.random()*goodStuff.length)];
-  document.getElementById("compliments").innerHTML = "<div>"+"You"+" "+did+" "+" "+what+" "+when;
+  document.getElementById("compliments").innerHTML = "<div>"+"I think"+" "+"you"+" "+how+" "+did+" "+" "+what+" "+"</div>";
 }
+
